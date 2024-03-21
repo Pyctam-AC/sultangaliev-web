2c5349c3d29a33e2c5636d15353171fa239cc779
Rustam Sultangaliev <sultangaliev-ru@yandex.ru>
2024-03-22 01:13:06 +0500

    feat: created HeadingIntro & fix style

---
+++ src/components/HeadingIntro/page/data.ts
@@ -0,0 +1,14 @@
+import { textList } from "../../../interface/textContent";
+
+export const textIntro:textList = {
+  EN: {
+    first_Line: "Hi,",
+    second_Line: "I'm Rustam,",
+    third_Line: "web developer",
+  },
+  RU: {
+    first_Line: "Привет,",
+    second_Line: "я Рустам,",
+    third_Line: "web разработчик",
+  }
+}
