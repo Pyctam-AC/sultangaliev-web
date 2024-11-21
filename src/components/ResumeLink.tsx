5d1f5b88cc03287c94754511a36a65f387c68741
Rustam Sultangaliev <sultangaliev-ru@yandex.ru>
2024-11-21 22:32:07 +0500

    create: new resume & VC

--- src/components/ResumeLink.tsx
+++ src/components/ResumeLink.tsx
@@ -1,14 +1,26 @@
 import { FC, useContext } from 'react';
-import resumePdf from '../docs/Rustam_Sultangaliev_resume.pdf';
+import CV from '../docs/Rustam_Sultangaliev_CV.pdf';
+import resume from '../docs/Рустам_Султангалиев_резюме.pdf'
 import LangContexts from '../contexts/LangContexts';
 import {textsResume} from '../utils/textsPage.ts';
 
+type Language = 'EN' | 'RU';
+
 const ResumeLink:FC = () => {
 
-  const lang = useContext(LangContexts);
+  const lang = useContext(LangContexts) as Language;
+
+  const linkRezume = {
+    EN: {
+      link: CV
+    },
+    RU: {
+      link: resume
+    }
+  }
 
   return (
-    <a href={resumePdf} target="_blank" rel="noreferrer">
+    <a href={linkRezume[lang].link} target="_blank" rel="noreferrer">
       <div className="resume-link">
         <p>{textsResume[lang].text}</p>
       </div>
